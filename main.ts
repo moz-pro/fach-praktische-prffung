228d5649940cbbc45c8c6a776c2a025cd3284f5f
moz-pro <mozpro@outlook.de>
2025-01-20 08:43:37 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
 basic.forever(function () {
-	
+    if (pins.analogReadPin(AnalogPin.P0) < 30) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    } else if (pins.analogReadPin(AnalogPin.P0) > 25) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    } else if (pins.analogReadPin(AnalogPin.P0) > 25 && pins.analogReadPin(AnalogPin.P0) < 30) {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
 })
